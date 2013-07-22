c25fbce0ad12fa7b7f1edbb28be29cb085492f50
Zhang Shibing <shibingzhang@sohu-inc.com>
2013-07-22 14:08:57 +0800

    add router.json

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var app = require('express')(),
-	fs = require('fs');
+  fs = require('fs');
 
 app.configure(function() {
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
 });
 
-app.get('/', function(req, res) {
-	res.render('index', {
-		title: '首页'
-	});
-});
+var router = JSON.parse(fs.readFileSync('config/router.json', 'utf-8'));
 
 app.get('*', function(req, res) {
-	res.render('404');
+  if (router[req.url]) {
+  	res.render(router[req.url]);
+  } else {
+  	res.render('404');
+  }
 });
 
 app.listen('3000');
