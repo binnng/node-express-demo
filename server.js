19d4c951484e3924d8e8f41ac6efaf3ecfcdf92c
Zhang Shibing <shibingzhang@sohu-inc.com>
2013-07-23 19:38:53 +0800

    add stylus & jade

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-var app = require('express')(),
-  fs = require('fs');
+var express = require('express'),
+  app = express(),
+  fs = require('fs'),
+  stylus = require('stylus');
 
 app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.favicon());
+  //生产环境
+  //app.set('env', 'production');
 });
 
-var router = JSON.parse(fs.readFileSync('config/router.json', 'utf-8'));
+var router = JSON.parse(fs.readFileSync(__dirname + '/config/router.json', 'utf-8'));
+
+//static files
+if ('development' === app.get('env')) {
+
+  app.get(/css$/, function(req, res) {
+
+    stylus(fs.readFileSync(__dirname + req.url.replace('css', 'styl'), 'utf8'))
+    .set('filename', __dirname + req.url)
+    .render(function(err, css){
+      if (err) throw err;
+  	  res.set({
+  	    'Content-Type': 'text/css',
+  	    'Content-Length': css.length
+  	  });
+      res.send(css);
+    });
+    
+  });
+
+  app.get(/js$/, function(req, res) {
+      res.send('js');
+  });
+
+}
 
 app.get('*', function(req, res) {
   res.render(router[req.url] || '404');
